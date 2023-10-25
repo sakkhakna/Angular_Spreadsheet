84128465e39cfe6c6da974766e0dae92dc5704c1
Sak Khakna <sthissakkhakna@gmail.com>
2023-10-25 14:14:35 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {CellStyleModel, getFormatFromType} from "@syncfusion/ej2-angular-spreadsheet";
+import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
+import { LocalData } from "./data";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularSpreadsheet';
+  public cloudDate:object = new DataManager({
+    url:'https://services.syncfusion.com/angular/production/api/Orders',
+    adaptor: new WebApiAdaptor(),
+    crossDomain: true
+  })
+  public btnClick() {
+    this.cloudDate = LocalData;
+  }
+
+  public cellStyle: CellStyleModel = {fontWeight: 'bold'}
+  public boldCenter = { fontWeight: 'bold', textAlign: 'center'}
+  public currencyFormat: string = getFormatFromType('Currency');
+  public data: Object[] = [{
+    "Student Name": "Sak Khakna",
+    "Gender": "Male",
+    "Birthday": "15/11/2003",
+    "Major": "ITE",
+    "Class": "M4",
+    "Paid": "123.15",
+  },
+    {
+      "Student Name": "Sok Sok",
+      "Gender": "Female",
+      "Birthday": "15/12/2012",
+      "Major": "ITE",
+      "Class": "M4",
+      "Paid": "345.15",
+    },
+  ]
 }
